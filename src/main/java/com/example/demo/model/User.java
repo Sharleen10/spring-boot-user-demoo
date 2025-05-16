c6f5abb2311ff0263b4d3ba3e8a421d19273ba67
Sharleen10 <sharleensshabangu@gmail.com>
2025-05-16 14:34:16 +0200

    feat(model): add default and parameterized constructors

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -5,5 +5,22 @@ public class User {
     private String name;
     private String surname;
 
+    /**
+     * Default constructor
+     */
+    public User() {
+    }
 
+    /**
+     * Parameterized constructor
+     *
+     * @param id The user's ID
+     * @param name The user's first name
+     * @param surname The user's last name
+     */
+    public User(long id, String name, String surname) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+    }
 }
